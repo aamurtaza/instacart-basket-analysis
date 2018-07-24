f7d5bc4b1c811b52946699b9fd906d5e2e685d02
adnanalimurtaza <adnan.ali.murtaza@gmail.com>
2018-07-25 02:55:49 +0300

    find best selling products

--- src/01_eda_instacart.py
+++ src/01_eda_instacart.py
@@ -16,6 +16,25 @@ def load_data():
     return data_in_dict
 
 
+def n_products(data_frame, n=10, view_type='head'):
+    """ Finds the top or bottom n products in orders/sales.
+
+    :param data_frame: DataFrame containing order_products data.
+    :param n: number of products
+    :param view_type: Defines the type either head or tail.
+    :return: DataFrame of top or bottom n products
+    """
+    grouped = data_frame.groupby('product_id')
+    count = grouped.size().reset_index(name='order_id_count')
+    sort_count = count.sort_values(by='order_id_count', ascending=False)
+    if view_type is 'head':
+        return sort_count.head(n)
+    elif view_type is 'tail':
+        return sort_count.tail(n)
+    else:
+        print('view_type can either be head or tail.')
+
+
 if __name__ == '__main__':
     # load data into data frames
     data_frames = load_data()
@@ -24,3 +43,9 @@ if __name__ == '__main__':
               'https://www.instacart.com/datasets/grocery-shopping-2017')
         exit(0)
 
+    # data frames
+    order_products = data_frames['order_products_train']
+    orders = data_frames['orders']
+
+    # find top n best selling products
+    top_n_products = n_products(order_products.iloc[:, :], view_type='head')
